5a9fc474d47f8edebd1259f644c5fb55016040e0
procode99 <web.poran@gmail.com>
2025-07-27 00:05:15 +0600

    this is a completed simple javascript

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 var  name ="shah paran";
-console.log(name)
+console.log(name);
+ 
+var address = "sakhipur";
+var country = "Bangladesh";
+var bangladesh = true;
+ console.log(bangladesh);
+ // true or fasls variabne all is buliean type variable   
+var passed = false;
+console.log(passed);
+passed = true;
+console.log( typeof passed);
+console.log(country);
+var price = 10000;
+console.log(price);
+console.log(typeof price);
+var more_price = "50000"
+console.log(more_price);
+console.log(typeof more_price);
