ae0e6061898e1c35452d20b53589613d7783c197
procode99 <web.poran@gmail.com>
2025-07-27 07:21:54 +0600

    this is completed 1st task

---
+++ pracktice.js
@@ -0,0 +1,7 @@
+var applePrice = 350;
+var orangePrice = 450 ;
+ var totalprice = applePrice +orangePrice;
+console.log('My total buing price is ' + totalprice);
+var myBlance = 2000;
+var myNewBlanceIs = myBlance- totalprice;
+console.log('my blance is' + myNewBlanceIs)
