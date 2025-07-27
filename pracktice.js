187195b051f60e59d18273ca1bba26f7d0fa6f1a
procode99 <web.poran@gmail.com>
2025-07-27 07:35:53 +0600

    completed 2nd task

--- pracktice.js
+++ pracktice.js
@@ -4,4 +4,4 @@ var orangePrice = 450 ;
 console.log('My total buing price is ' + totalprice);
 var myBlance = 2000;
 var myNewBlanceIs = myBlance- totalprice;
-console.log('my blance is' + myNewBlanceIs)
+console.log('my blance is' + myNewBlanceIs);
---
+++ pracktice2.js
@@ -0,0 +1,11 @@
+// this is a avarage marks read part
+
+var math = 75;
+var biology =25;
+var chemistry = 65;
+var physics = 80;
+var bangla = 35;
+var totalNumber = math + biology + chemistry + physics + bangla;
+console.log(totalNumber);
+var avarecges = totalNumber/5;
+console.log('This is my avarage number' + avarecges );
