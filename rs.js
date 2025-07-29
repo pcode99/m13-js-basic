4d95672e92ca27c856e82e1b1db1ec628fb6ae91
procode99 <web.poran@gmail.com>
2025-07-29 10:28:09 +0600

    this is a commit for javas ript

--- rs.js
+++ rs.js
@@ -1,23 +1,25 @@
-const age = 30;
+const age = 15;
 const price = 500;
- if (age < 12){
-    console.log("you can eat for feree")
- }
- else if (age >=60 ){
-    //50% discount 
+//  if (age < 12){
+//     console.log("you can eat for feree")
+//  }
+//  else if (age >=60 ){
+//     //50% discount 
 
-    const discount = price * 50/100;
-    const payAmount = price - discount;
-    console.log(payAmount);
+//     const discount = price * 50/100;
+//     const payAmount = price - discount;
+//     console.log(payAmount);
 
   
- }
- else if (age <=30 || age <=60){
-    //12% add many
-    const addMany = price * 18 /100;
-    const payAmount = price + addMany;
-    console.log(payAmount)
- }
-   else{
-        console.log(price);
-    }
+//  }
+//  else if (age <=30 || age <=60){
+//     //12% add many
+//     const addMany = price * 18 /100;
+//     const payAmount = price + addMany;
+//     console.log(payAmount)
+//  }
+//    else{
+//         console.log(price);
+//   a
+
+age>=20? console.log("tomar age 20 ar besi"):console.log("tomar boyos 20 ar kom ");
